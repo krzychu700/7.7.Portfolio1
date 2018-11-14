943b6a93ce3d424b34d923ecf61cc7c89d35056f
Krzysztof Szypulski <krzychu700@gmail.com>
2018-11-14 20:59:13 +0100

    task finished

--- js/about.js
+++ js/about.js
@@ -1,6 +1,39 @@
-const aboutPos = document.querySelector(".about");
+const aboutPos = document.querySelector(".about").offsetTop;
+const teamPos = document.querySelector(".team").offsetTop;
+const portfolioPos = document.querySelector(".portfolio").offsetTop;
+const boxesAbout = document.querySelectorAll(".about_date");
+const boxesPortfolio = document.querySelectorAll(".portfolio__box");
+const boxLine = document.querySelector(".about__line");
 
+window.addEventListener('scroll', function () {
+  let windowScroll = window.scrollY;
 
-window.addEventListener('scroll', function() {
-  console.log(window.scrollY)
+  boxesAbout.forEach(function (boxAbout) {
+    if (windowScroll > aboutPos + boxAbout.offsetTop - window.innerHeight * .7) {
+      boxAbout.style.opacity = 1;
+    } else {
+      boxAbout.style.opacity = 0;
+    }
+  })
+
+  function showLine() {
+    let lineHeight = 0;
+    if ((windowScroll - aboutPos > 0) && (window.screen.availWidth > 426)) {
+      boxLine.style.opacity = 1;
+      boxLine.style.height = `${lineHeight+windowScroll-aboutPos}px`;
+    } else {
+      boxLine.style.height = 0;
+      boxLine.style.opacity = 0;
+    }
+  }
+
+  showLine()
+
+  boxesPortfolio.forEach(function (boxPortfolio) {
+    if (windowScroll > boxPortfolio.offsetTop - window.innerHeight * .7) {
+      boxPortfolio.style.opacity = 1
+    } else {
+      boxPortfolio.style.opacity = 0
+    }
+  })
 });
